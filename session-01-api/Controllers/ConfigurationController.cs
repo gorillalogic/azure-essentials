7dd8bcce223d794a1fdff79a06b244270eb9bd88
Francisco Mac Courtney <francisco.maccourtney@gorillalogic.com>
2019-10-01 09:22:16 -0600

    Adding configuration keys and new controller.

---
+++ session-01-api/Controllers/ConfigurationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace session01api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConfigurationController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public ConfigurationController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return Configuration["MyCustomKey"];
+        }
+    }
+}
