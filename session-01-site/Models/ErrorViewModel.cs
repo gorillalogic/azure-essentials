b260a263b47ad617681ba1a5efd3938052781681
Francisco Mac Courtney <francisco.maccourtney@gorillalogic.com>
2019-10-01 11:05:28 -0600

    Adding new site project

---
+++ session-01-site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace session_01_site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
